9ee109ffd86391125eb6a31a5ceaf27e774c4351
Matthew Puzey <matthew.puzey@skybettingandgaming.com>
2019-09-09 20:37:26 +0100

    'calculate' 0 score

--- bowling/first_attempt/bowling.go
+++ bowling/first_attempt/bowling.go
@@ -5,3 +5,10 @@ type Game struct{}
 func New() *Game {
 	return &Game{}
 }
+
+func (g *Game) Roll(pins int) {
+}
+
+func (g *Game) Score() int {
+	return 0
+}
--- bowling/first_attempt/bowling_test.go
+++ bowling/first_attempt/bowling_test.go
@@ -8,6 +8,15 @@ import (
 func TestGutterGame(t *testing.T) {
 	game := bowling_game.New()
 	if game == nil {
-		t.Fatalf("")
+		t.Fatalf("nil game created")
+	}
+
+	for i := 1; i <= 20; i++ {
+		game.Roll(0)
+	}
+
+	score := game.Score()
+	if score != 0 {
+		t.Fatalf("score %s is incorrect", string(score))
 	}
 }
