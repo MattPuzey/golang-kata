3672c88d7b7388c2f2dc71435723df8e90ce9e25
Matthew Puzey <matthew.puzey@skybettingandgaming.com>
2019-09-09 20:50:25 +0100

    last test passes

--- bowling/first_attempt/bowling.go
+++ bowling/first_attempt/bowling.go
@@ -1,14 +1,19 @@
 package bowling
 
-type Game struct{}
+type Game struct {
+	score int
+}
 
 func New() *Game {
 	return &Game{}
 }
 
 func (g *Game) Roll(pins int) {
+	if pins != 0 {
+		g.score += 1
+	}
 }
 
 func (g *Game) Score() int {
-	return 0
+	return g.score
 }
