42d19d6b0483ab7bb25ac817db44cee81eb3b510
Matthew Puzey <matthew.puzey@skybettingandgaming.com>
2019-09-13 13:42:09 +0100

    red - missing new()

---
+++ bowling/second_attempt/bowling.go
@@ -0,0 +1,4 @@
+package bowling
+
+type Game struct {
+}
---
+++ bowling/second_attempt/bowling_test.go
@@ -0,0 +1,11 @@
+package bowling_test
+
+import (
+	bowling_game "kata/bowling/second_attempt"
+	"testing"
+)
+
+func TestGutterGame(t *testing.T) {
+	game := bowling_game.New()
+
+}
